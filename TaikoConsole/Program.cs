0b8a13938945757336141920f91f90d623470a36
Fudgey <fudgebucket27@gmail.com>
2023-02-15 16:30:42 +1000

    Get chain id working

--- TaikoConsole/Program.cs
+++ TaikoConsole/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using TaikoSharp.Services;
+TaikoL2Client taikoL2Client = new TaikoL2Client();
+var chainID = await taikoL2Client.GetChainIDAsync();
+Console.WriteLine($"Taiko L2 Chain ID: {chainID}");
+Console.WriteLine("Enter to exit:");
+Console.ReadLine();
--- TaikoSharp/Services/TaikoL2Client.cs
+++ TaikoSharp/Services/TaikoL2Client.cs
@@ -1,14 +1,28 @@
-﻿using System;
+﻿using Nethereum.JsonRpc.Client;
+using Nethereum.Signer;
+using Nethereum.Web3;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using TaikoSharp.Helpers;
+using TaikoSharp.Models;
 
 namespace TaikoSharp.Services
 {
-    public class TaikoL2Client
+    public class TaikoL2Client : ITaikoL2Client
     {
+        readonly RpcClient RpcClient;  
         public TaikoL2Client()
         {
+            RpcClient = new RpcClient(new Uri(Constants.L2RPCUrl));
+        }
 
+        public async Task<int> GetChainIDAsync()
+        {
+           RpcRequest rpcRequest = new RpcRequest(0, "eth_chainId");
+           string rpcResponse = await RpcClient.SendRequestAsync<string>(rpcRequest);
+           return int.Parse(rpcResponse.Substring(2), System.Globalization.NumberStyles.HexNumber);
         }
     }
 }
