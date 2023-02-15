5cab47609e02ee57e9d4694893911d2df8075fa5
Fudgey <fudgebucket27@gmail.com>
2023-02-15 15:29:13 +1000

    Rename variables. We already know this is for Taiko!

--- TaikoSharp/Helpers/Constants.cs
+++ TaikoSharp/Helpers/Constants.cs
@@ -5,15 +5,15 @@ namespace TaikoSharp.Helpers
     public static class Constants
     {
         //L1
-        public const string TaikoL1ContractAddress = "0x7B3AF414448ba906f02a1CA307C56c4ADFF27ce7";
-        public const string TaikoL1TokenVault = "0xD0dfd5baCf160B97C8eE3ecb463F18c08673160c";
-        public const string TaikoL1Bridge = "0x3612E284D763f42f5E4CB72B1602b23DAEC3cA60";
-        public const string TaikoL1Horse = "0x3E3a3416DbCc5cb4448B6b171fF15f9Da35Ab72d";
+        public const string L1ContractAddress = "0x7B3AF414448ba906f02a1CA307C56c4ADFF27ce7";
+        public const string L1TokenVault = "0xD0dfd5baCf160B97C8eE3ecb463F18c08673160c";
+        public const string L1Bridge = "0x3612E284D763f42f5E4CB72B1602b23DAEC3cA60";
+        public const string L1Horse = "0x3E3a3416DbCc5cb4448B6b171fF15f9Da35Ab72d";
         //L2
-        public const string TaikoL2ContractAddress = "0x0000777700000000000000000000000000000001";
-        public const string TaikoL2TokenVault = "0x0000777700000000000000000000000000000002";
-        public const string TaikoL2EtherVault = "0x0000777700000000000000000000000000000003";
-        public const string TaikoL2Bridge = "0x0000777700000000000000000000000000000004";
-        public const string TaikoL2Horse = "0xb8ea36284d1fa062cc0c969e28a49f3531cfb1bf";
+        public const string L2ContractAddress = "0x0000777700000000000000000000000000000001";
+        public const string L2TokenVault = "0x0000777700000000000000000000000000000002";
+        public const string L2EtherVault = "0x0000777700000000000000000000000000000003";
+        public const string L2Bridge = "0x0000777700000000000000000000000000000004";
+        public const string L2Horse = "0xb8ea36284d1fa062cc0c969e28a49f3531cfb1bf";
     }
 }
