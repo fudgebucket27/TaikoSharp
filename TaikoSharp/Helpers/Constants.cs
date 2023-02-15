3561646eab4571869a8b261a064e8b8c08d44d7b
Fudgey <fudgebucket27@gmail.com>
2023-02-15 15:33:38 +1000

    add rpc urls and chain id

--- TaikoSharp/Helpers/Constants.cs
+++ TaikoSharp/Helpers/Constants.cs
@@ -9,11 +9,15 @@ namespace TaikoSharp.Helpers
         public const string L1TokenVault = "0xD0dfd5baCf160B97C8eE3ecb463F18c08673160c";
         public const string L1Bridge = "0x3612E284D763f42f5E4CB72B1602b23DAEC3cA60";
         public const string L1Horse = "0x3E3a3416DbCc5cb4448B6b171fF15f9Da35Ab72d";
+        public const string L1ChainID = "31338";
         //L2
         public const string L2ContractAddress = "0x0000777700000000000000000000000000000001";
         public const string L2TokenVault = "0x0000777700000000000000000000000000000002";
         public const string L2EtherVault = "0x0000777700000000000000000000000000000003";
         public const string L2Bridge = "0x0000777700000000000000000000000000000004";
         public const string L2Horse = "0xb8ea36284d1fa062cc0c969e28a49f3531cfb1bf";
+        public const string L2RPC = "https://l2rpc.a1.taiko.xyz";
+        public const string L2ChainID = "167003";
+
     }
 }
