b0ccdd88cd447f4f652ab231fe1d912002d0cf4e
Fudgey <fudgebucket27@gmail.com>
2023-02-15 15:45:24 +1000

    client classes

---
+++ TaikoSharp/Services/TaikoL1Client.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaikoSharp.Services
+{
+    public class TaikoL1Client
+    {
+        public TaikoL1Client()
+        {
+
+        }
+    }
+}
---
+++ TaikoSharp/Services/TaikoL2Client.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaikoSharp.Services
+{
+    public class TaikoL2Client
+    {
+        public TaikoL2Client()
+        {
+
+        }
+    }
+}
